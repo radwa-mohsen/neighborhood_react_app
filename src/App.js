b210fa0fe4b4e3a023bdb611ffb8f471fa6c0b21
radwa-mohsen <radwa@gfxway.com>
2018-09-22 13:02:51 +0200

    show content from api

--- src/App.js
+++ src/App.js
@@ -9,13 +9,41 @@ class App extends Component {
   state = {
     className : "available-locations",
     locations :[
-      {title: "San Stifano Hotel" ,location :{lat:31.247011 ,lng:29.969717299999957 }},
-      {title:"Agamy Star Mall" ,location :{lat:31.110298 ,lng:29.78775070000006 }},
-      {title:"Montaza Palace" ,location :{lat:  31.2884965,lng:30.015969600000062 }},
-      {title: "Bibliotheca Alexandrina",location :{lat:31.2088705 ,lng: 29.909201199999984}},
-      {title: "City Square Mall",location :{lat: 31.2281625,lng:29.94211210000003}},
-      {title:"Green Plaza Mall" ,location :{lat: 31.2066635,lng:29.965166899999986}},
-      {title: "DownTown Mall",location :{lat: 31.1684721,lng:29.936801599999967}}
+      {
+        title: "Alexandria Opera House" ,
+        location :{lat:31.19675264670403 ,lng:29.90200721772115 },
+        venue_id:"4dcd8b71d164679b8ce935c3"
+      },
+      {
+        title:"Citadel of Qaitbay" ,
+        location :{lat:31.212945482937627 ,lng:29.884037201555333},
+        venue_id:"4ca062acd3c2b60cb673dcbc"
+      },
+      {
+        title:"Montaza Palace" ,
+        location :{lat:31.288493334799238,lng:30.014485875619275 },
+        venue_id:"501c8c3ae4b067f6ac7166a1"
+      },
+      {
+        title: "Bibliotheca Alexandria",
+        location :{lat:31.2088705 ,lng: 29.909201199999984},
+        venue_id:"4bd5cd885631c9b6edbaa430"
+      },
+      
+      {
+        title: "Royal Jewelry Museum",
+        location :{lat: 31.23913421499753,lng:29.96291810417218},
+        venue_id:"4f53c281e4b033653577293d"
+      },
+      {
+        title:"Green Plaza Mall" ,
+        location :{lat: 31.206731838220897,lng:29.964734612113496},
+        venue_id:"52dc1bed498ea9c9d5a148b2"},
+      {
+        title: "City Centre Mall",
+        location :{lat: 31.168552010633217,lng:29.932185759584424},
+        venue_id:"4b4b88f8f964a520769f26e3"
+      }
     ]
   }
 
@@ -29,7 +57,6 @@ class App extends Component {
   initMap = ()=>{
     
     const {locations} = this.state
-    //const {chosenLocation} = this.state
     const google = window.google
   let usedLocations
 
@@ -62,10 +89,43 @@ class App extends Component {
     marker.addListener('click',function(){
         populateInfoWindow(this,largeInfowindow)
     })
-    function populateInfoWindow (marker,infoWindow){
+    let populateInfoWindow = (marker,infoWindow)=>{
+      const {locations} = this.state
+      let location = locations.filter(loc=>loc.title === marker.title)
      if (infoWindow.marker !== marker) {
          infoWindow.marker = marker;
-         infoWindow.setContent(`<div><a href="#">  ${marker.title}</a> </div>`);
+         let clientID = "5TKOPJNR2M5EWQAQK23IB4NU31SP1NYM2UBI13LOYAIVOIVA"
+         let clientSecret = "OGVH2HFI3VGFWM2I5DG2MEO13P25133A5UERAX4SRDQTUGYT"
+        //  let clientID = "ULJMAZZVQKOP0P54O1ZS1SJGFWTUGBC4DRPY4HQ24MHA1ZBR";
+        //  let clientSecret ="YGKRI11UK20KMZQXO3XIYDTP0XEPHC2B5NHSYL34LWHD0CND";
+         
+         let url = "https://api.foursquare.com/v2/venues/"+ location[0].venue_id + "?client_id="+clientID +"&client_secret="+clientSecret+"&v=20130815"
+         fetch(url).then((response)=>{
+          if (response.status !== 200) {
+            infoWindow.setContent("<p>Sorry information about that place can't be loaded .. please try again later<p>");
+            return;
+        }
+               response.json().then((info)=>{
+                 
+                 let venue = info.response.venue
+                 let image = venue.bestPhoto
+                 let imageURL = image.prefix + "100x100" + image.suffix
+                 let name = venue.name
+                 let address = venue.location.formattedAddress
+                 let html = `<div class="content">
+                              <div>
+                                <h3>${name}</h3>
+                                <p>${address}</p>
+                                <a href="https://foursquare.com/v/${venue.id}">Read More</a>
+                              </div>
+                              <img src="${imageURL}" alt="${name}"/>
+                            </div>`
+                
+                 infoWindow.setContent(html)
+               })
+         }).catch(()=>{
+          infoWindow.setContent("<p>Sorry information about that place can't be loaded<p>");
+         })
          infoWindow.open(map,marker);
          infoWindow.addListener('closeclick',function(){
              infoWindow.setContent(null)
@@ -81,8 +141,6 @@ class App extends Component {
     markers.map(marker=>marker.setMap(null))
     chosenLocation.map(loc=>
       markers.filter((marker)=>marker.title === loc.title)[0].setMap(map))
-  
- 
   }
 
   openContent = (location)=>{
